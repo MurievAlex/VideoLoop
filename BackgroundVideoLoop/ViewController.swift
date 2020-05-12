b2fc62e64533df0e4fa652b3030f2331d9a55821
AsahiOcean <sergfedotov@icloud.com>
2020-05-12 17:07:09 +0300

    Initial Commit

---
+++ BackgroundVideoLoop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundVideoLoop
+//
+//  Created by Serg Fedotov on 12.05.2020.
+//  Copyright © 2020 Sergey Fedotov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
